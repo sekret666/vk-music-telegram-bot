05f64e880ca1f20c85c5125eb23ba5a73f19171c
sekret666 <66795778+sekret666@users.noreply.github.com>
2021-02-01 17:23:42 +0400

    Update config.py

--- tgdbassbot/config.py
+++ tgdbassbot/config.py
@@ -1,4 +1,4 @@
-API_TOKEN = "TOKEN"
+API_TOKEN = "1615610481:AAHPlLTfaFHYBo2h9Ot4IoxDG-W8avU6wnQ"
 
 commands = ['/start', '/song', '/artist', '/setlang',
             '/settings', '/my', '/users', '/newpost']
